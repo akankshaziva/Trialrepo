32ed72ba4d2b870af6d1767934d3af33351aa8f5
Akanksha Kadvekar <91193956+akankshaziva@users.noreply.github.com>
2022-02-10 14:06:33 +0530

    Update trialcode.py

--- trialcode.py
+++ trialcode.py
@@ -1,5 +1,22 @@
-def getBuildUser() :
-    return currentBuild.rawBuild.getCause(Cause.UserIdCause).getUserId()
-    
-
-getBuildUser()
+import jenkins.model.Jenkins
+name = "test_master_builder_project"
+//If we want to add more then one job
+def items = new LinkedHashSet();
+def job = Hudson.instance.getJob(name)
+items.add(job);
+items.each { item ->
+    try {
+        def job_data = Jenkins.instance.getItemByFullName(item.fullName)
+        println 'Job: ' + item.fullName
+        if (job_data.getLastBuild()) {
+            last_job_num = job_data.getLastBuild().getNumber()
+            def upStreamBuild = Jenkins.getInstance().getItemByFullName(item.fullName).getBuildByNumber(last_job_num)
+println 'LastBuildNumer: ' + last_job_num
+            println "LastBuildTime: ${upStreamBuild.getTime()}"
+        } else {
+            println 'LastBuildNumer: Null'
+        }
+    } catch (Exception e) {
+        println ' Ignoring exception ' + e
+    }
+}
