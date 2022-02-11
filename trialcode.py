562c866112c10baf6eeddd34d1cf99ec009dcc4f
Akanksha Kadvekar <91193956+akankshaziva@users.noreply.github.com>
2022-02-11 11:54:12 +0530

    Update trialcode.py

--- trialcode.py
+++ trialcode.py
@@ -1,22 +1,43 @@
+from json import dumps
+
+from httplib2 import Http
+
 import jenkins.model.Jenkins
-name = "test_master_builder_project"
-//If we want to add more then one job
-def items = new LinkedHashSet();
-def job = Hudson.instance.getJob(name)
-items.add(job);
-items.each { item ->
-    try {
-        def job_data = Jenkins.instance.getItemByFullName(item.fullName)
-        println 'Job: ' + item.fullName
-        if (job_data.getLastBuild()) {
-            last_job_num = job_data.getLastBuild().getNumber()
-            def upStreamBuild = Jenkins.getInstance().getItemByFullName(item.fullName).getBuildByNumber(last_job_num)
-println 'LastBuildNumer: ' + last_job_num
-            println "LastBuildTime: ${upStreamBuild.getTime()}"
-        } else {
-            println 'LastBuildNumer: Null'
-        }
-    } catch (Exception e) {
-        println ' Ignoring exception ' + e
-    }
-}
+
+
+
+def main():
+    """Google Chat incoming webhook quickstart."""
+    url = 'https://chat.googleapis.com/v1/spaces/AAAARM7dFAE/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=73W5Cltjjjv3IGdUv0MWoMP8eegMavqPu3HpAYTeL5I%3D'
+    #bot_message = {'text' :'Hello from a Python script!'}
+        
+    bot_message = """ BUILD_USER= ${BUILD_USER}
+                  BUILD_USER_ID =${BUILD_USER_ID}
+                  BUILD_USER_EMAIL=${BUILD_USER_EMAIL}"
+                  BUILD_NUMBER= ${BUILD_NUMBER}
+    """    
+    
+        
+
+    message_headers = {'Content-Type': 'application/json; charset=UTF-8'}
+
+    http_obj = Http()
+
+    response = http_obj.request(
+        uri=url,
+        method='POST',
+        headers=message_headers,
+        #body=dumps(bot_message),
+        body=bot_message
+        
+    )
+
+    print(response)
+ 
+ 
+
+if __name__ == '__main__':
+    main()
+    
+    
+ 
